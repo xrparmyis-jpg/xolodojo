b36acab879b808628f2b3ea5de78ea75ad280395
Todd Nagel <todd.alan.nagel@gmail.com>
2026-05-10 01:51:17 -0700

    test

--- src/components/SectionParallaxBlobs.tsx
+++ src/components/SectionParallaxBlobs.tsx
@@ -4,6 +4,12 @@ interface SectionParallaxBlobsProps {
     colorBg2Offset: number;
 }
 
+const blobMotion =
+    'will-change-transform transition-transform duration-100 ease-out';
+/** Below `lg`, blobs read as overpowering behind body copy; keep full punch on large screens. */
+const blobVisual =
+    'pointer-events-none brightness-[0.56] saturate-[0.82] md:brightness-[0.68] md:saturate-[0.88] lg:brightness-[1.2] lg:saturate-100';
+
 function SectionParallaxBlobs({
     bgShapeOffset,
     colorBgOffset,
@@ -12,21 +18,21 @@ function SectionParallaxBlobs({
     return (
         <>
             <div
-                className="absolute inset-y-0 left-1/2 -z-10 pointer-events-none brightness-[1.2] will-change-transform transition-transform duration-100 ease-out"
+                className={`absolute inset-y-0 left-1/2 -z-10 ${blobVisual} ${blobMotion}`}
                 style={{ transform: `translateX(-50%) translateY(${bgShapeOffset}px)` }}
             >
                 <img src="/bg-shape.png" alt="shape-img" />
             </div>
 
             <div
-                className="absolute bottom-1/4 left-0 -z-10 pointer-events-none brightness-[1.2] will-change-transform transition-transform duration-100 ease-out"
+                className={`absolute bottom-1/4 left-0 -z-10 ${blobVisual} ${blobMotion}`}
                 style={{ transform: `translateY(${colorBgOffset}px)` }}
             >
                 <img src="/color-bg-shape.png" alt="img" />
             </div>
 
             <div
-                className="absolute right-[-100px] top-[-12%] -z-10 pointer-events-none brightness-[1.2] will-change-transform transition-transform duration-100 ease-out"
+                className={`absolute right-[-100px] top-[-12%] -z-10 ${blobVisual} ${blobMotion}`}
                 style={{ transform: `translateY(${colorBg2Offset}px)` }}
             >
                 <img src="/color-bg-shape-2.png" alt="img" />
